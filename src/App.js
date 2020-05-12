7b470f5144ac1c11636d14325b73eecde4826b13
jsjtxietian <jsjtxietian@outlook.com>
2020-05-13 00:24:17 +0800

    8

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import bg3 from './Img/3.png';
 import bg5 from './Img/5.png';
 import bg6 from './Img/6.png';
 import bg7 from './Img/7.png';
+import bg8 from './Img/8.png';
 
 
 import bt2 from './Img/bt2.png';
@@ -19,6 +20,8 @@ import bt65 from './Img/bt65.png';
 import bt66 from './Img/bt66.png';
 import bt71 from './Img/bt71.png';
 import bt72 from './Img/bt72.png';
+import bt81 from './Img/bt81.png';
+import bt82 from './Img/bt82.png';
 
 
 
@@ -148,6 +151,31 @@ function App() {
 					</img>
 				</div>
 			</div>
+
+			<div className="Comic" style={{ backgroundImage: `url(${bg8})` }}>
+				<div style={{ position: "absolute", "left": "408px", "top": "101px" }}>
+					<img
+						src={bt81}
+						className="bt8"
+						onClick={s => {
+							setOpen(true);
+							setImg(bt2);//change here
+						}}>
+					</img>
+				</div>
+
+				<div style={{ position: "absolute", "left": "408px", "top": "347px" }}>
+					<img
+						src={bt82}
+						className="bt8"
+						onClick={s => {
+							setOpen(true);
+							setImg(bt2);//change here
+						}}>
+					</img>
+				</div>
+			</div>
+
 		</div>
 	);
 }
